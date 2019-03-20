db6740a12cedaa367b31d07cf4d34b412f4f1083
Dr0kD3n <bkerikbk@gmail.com>
2019-03-20 14:04:50 +0300

    delete unnecessary fiels

--- ios1/build/Build/Intermediates.noindex/universes-mobile.build/Debug-iphonesimulator/universes-mobile.build/Script-B5722AD01DFB7E3F0084848F.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-pushd "${SRCROOT}/.."
-export PATH="$(if [ -f ~/.expo/PATH ]; then echo $PATH:$(cat ~/.expo/PATH); else echo $PATH; fi)"
-expo prepare-detached-build --platform ios
-popd
-
