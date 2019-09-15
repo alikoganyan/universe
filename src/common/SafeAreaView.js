81abf2b46d56a31660de0b88cc5f55b985ca9977
Alik Ohanyan <alik.oganyan@gmail.com>
2019-09-16 02:02:40 +0400

    fixes

--- src/common/SafeAreaView.js
+++ src/common/SafeAreaView.js
@@ -3,14 +3,16 @@ import { SafeAreaView, Platform, KeyboardAvoidingView } from 'react-native'
 // import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export default function SafeAreaViewComponent(props) {
-  const { children, style, behavior } = props
+  const { children, style } = props
   return (
-    <SafeAreaView style={{ ...style }}>
+    <SafeAreaView style={{ ...style, flex: 1 }}>
       {/* <KeyboardAvoidingView */}
       <KeyboardAvoidingView
-        scrollEnabled
-        behavior={behavior || 'position'}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : -243}
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={25}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'padding'}
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        scrollEnabled={false}
       >
         {children}
       </KeyboardAvoidingView>
