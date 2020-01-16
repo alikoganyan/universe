fef78e86d63e55a5f655ceda155ce41efcffadad
Vahe Ajamyan <vahe@iMac-imac.local>
2020-01-16 20:21:20 +0400

    style fix

--- src/common/OfflineNotice.js
+++ src/common/OfflineNotice.js
@@ -5,7 +5,15 @@ import ScreenLoader from './ScreenLoader'
 const { width } = Dimensions.get('window')
 function MiniOfflineSign(props) {
   return (
-    <View style={[styles.offlineContainer, { backgroundColor: props.bgColor }]}>
+    <View
+      style={[
+        styles.offlineContainer,
+        {
+          backgroundColor: props.bgColor,
+          top: Platform.OS === 'ios' && props.conntecionError ? 20 : 0,
+        },
+      ]}
+    >
       <Text style={styles.offlineText}>{props.text}</Text>
       <ScreenLoader color="red" />
     </View>
@@ -14,7 +22,11 @@ function MiniOfflineSign(props) {
 class OfflineNotice extends PureComponent {
   render() {
     return (
-      <MiniOfflineSign text={this.props.text} bgColor={this.props.bgColor} />
+      <MiniOfflineSign
+        text={this.props.text}
+        bgColor={this.props.bgColor}
+        conntecionError={this.props.conntecionError}
+      />
     )
   }
 }
@@ -26,7 +38,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     width,
     position: 'absolute',
-    top: Platform.OS === 'ios' ? 30 : 0,
     zIndex: 10,
   },
   offlineText: {
